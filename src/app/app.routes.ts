7a1fe7907a23c5b32fc960a2bc9307bab4656f93
KevinZav <kevinzavala.42@hotmail.com>
2021-10-29 14:58:25 -0500

    Agergando modulos y rutas a la aplicación

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,29 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { RouterModule, Routes } from '@angular/router'
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Grafica1Component } from './pages/grafica1/grafica1.component';
-import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
 import { PagesComponent } from './pages/pages.component';
 
 const routes: Routes = [
   {
-    path: '',
+    path: 'admin',
     component: PagesComponent,
-    children: [
-      { path: 'dashboard', component: DashboardComponent },
-      { path: 'progress', component: ProgressComponent },
-      { path: 'grafica1', component: Grafica1Component },
-      { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-    ],
+    loadChildren: () => import('./pages/pages.routing').then(m => m.PagesRoutingModule),
   },
-
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
-
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.routing').then(m => m.AuthRoutingModule),
+  },
+  { path: '', redirectTo: 'admin', pathMatch: 'full' },
   { path: '**', component: NopagefoundComponent },
 
 ];
